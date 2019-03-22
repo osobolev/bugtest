7a4cdfd3a801fa7a2abf5303786dc12e32a16509
oleg <sobolev_ov@mail.ru>
2019-03-22 12:14:29 +0300

    Вычисление хэша

--- src/main/java/ru/voskhod/bugs/dao/BugsDaoImpl.java
+++ src/main/java/ru/voskhod/bugs/dao/BugsDaoImpl.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import ru.voskhod.bugs.model.BugData;
 
 import javax.sql.DataSource;
+import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -44,7 +48,16 @@ public class BugsDaoImpl implements BugsDao {
 
     @Override
     public OptionalInt login(String login, String password) throws SQLException {
-        return null; // todo
+        MessageDigest sha256;
+        try {
+            sha256 = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException ex) {
+            throw new IllegalStateException(ex);
+        }
+        sha256.update((login + password).getBytes(StandardCharsets.UTF_8));
+        byte[] digest = sha256.digest();
+        String hash = DatatypeConverter.printHexBinary(digest);
+        return OptionalInt.empty(); // todo
     }
 
     @Override
