b8ede959bd7706df6f8ae199c928733b69303a68
kovalon <antonius95@yandex.ru>
2019-04-05 12:52:33 +0300

    NEW JS

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -1,7 +1,13 @@
-var vision = window.innerHeight;
-var indent = document.getElementsByClassName('header')[0].offsetHeight + 60 + 43;
-var height = vision - indent;
-var all_cols = document.getElementsByClassName('main-table-col-body');
-for (var i=0;i<all_cols.length; i++){
-    all_cols[i].style.height = height + "px";
+const a = () => {
+    var vision = window.innerHeight;
+    var indent = document.getElementsByClassName('header')[0].offsetHeight + 60 + 43;
+    var height = vision - indent;
+    var all_cols = document.getElementsByClassName('main-table-col-body');
+    for (var i=0;i<all_cols.length; i++){
+        all_cols[i].style.height = height + "px";
+    }
 }
+
+a();
+
+addEventListener('mouseover', a);
